ef99f3c643d19e617b74be03e0b788166a3ca209
almass <aboodalmass@altmails.com>
2025-03-20 08:49:40 +0300

    إضافة alsanea.onrender.com إلى ALLOWED_HOSTS

--- alsanea/settings.py
+++ alsanea/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1,alsanea.onrender.com').split(',')
 
 
 # Application definition
